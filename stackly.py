6d9f13c1a8a6b30a25e1f931f71b5ba6217114af
Danush Khanna <113158564+danushkhanna@users.noreply.github.com>
2023-04-08 04:14:48 -0700

    Update stackly.py

--- stackly.py
+++ stackly.py
@@ -3,6 +3,7 @@ import pandas as pd
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Set page configuration
 st.set_page_config(
     page_title="Stackly",
     page_icon=":books:",
@@ -12,11 +13,11 @@ st.set_page_config(
     backgroundColor="#FFFFFF",
     textColor="#262730",
     font="monospace",
+    # Change this to "light" if you prefer a light theme
     theme="dark"
 )
 
-# Page setup
-st.set_page_config(page_title="Stackly", page_icon="🐍", layout="wide")
+# Set page title
 st.title("Stackly - Learn Smart, Stack Fast")
 
 # Connect to the Google Sheet
@@ -25,10 +26,8 @@ sheet_id = "15ago01u5VzLpDuVJGcYG0cfflxKw9bVWoYphvrnFiUs"
 # Construct the URL to download the CSV file
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv"
 
+# Load the data into a pandas dataframe
 df = pd.read_csv(url, dtype=str).fillna("None")
-
-# Show the dataframe (we'll delete this later)
-# st.write(df)
 
 # Use a text_input to get the keywords to filter the dataframe
 text_search = st.text_input("Search videos by topics, course name, level or domain", value="")
@@ -40,32 +39,26 @@ m3 = df["Level"].str.contains(text_search)
 m4 = df["Domain"].str.contains(text_search)
 df_search = df[m1 | m2 | m3 | m4]
 
-# if text_search:
-#     st.write(df_search)
+# Show the filtered dataframe
+if not df_search.empty:
+    st.write(df_search)
+else:
+    st.write("No results found.")
 
 # Another way to show the filtered results
 # Show the cards
 N_cards_per_row = 3
-if text_search:
-    for n_row, row in df_search.reset_index().iterrows():
-        i = n_row%N_cards_per_row
-        if i==0:
+if not df_search.empty:
+    for n_row, row in df_search.iterrows():
+        i = n_row % N_cards_per_row
+        if i == 0:
             st.write("---")
             cols = st.columns(N_cards_per_row, gap="large")
         # draw the card
-
-        with cols[n_row%N_cards_per_row]:
+        with cols[i]:
             st.caption(f"{row['Course Name'].strip()}")
             st.caption(f"{row['University'].strip()}")
             st.caption(f"{row['Prerequisites'].strip()} ")
             st.markdown(f"**{row['Level'].strip()}**")
             st.markdown(f"*{row['Domain'].strip()}*")
             st.markdown(f"**{row['Links']}**")
-
-
-
-
-
-
-
-
